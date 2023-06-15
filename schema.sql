07cb7dc593b0883b06ca6b21556be7d59fffb6b0
{alexius <{alexiussavage0@gmail.com}>
2023-06-14 17:52:48 -0700

    added schema tables

--- schema.sql
+++ schema.sql
@@ -9,23 +9,42 @@ CREATE TABLE department(
 CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary VARCHAR(30) NOT NULL,
+    salary DECIMAL UNSIGNED NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30), NOT NULL,
     role_id INT NOT NULL,
     manager_id INT
 );
 
 USE employees;
-INSERT INTO department
-(name)
-VALUES
- ()
- ()
- ()
- ()
+INSERT INTO department (name)
+VALUES ('Accounting'),
+       ('Operations'),
+       ('Engineering'),
+       ('Sales');
+INSERT INTO role (title, salary, department_id,)
+VALUES ('Accounting manager',150000, 1),
+       ('Accountant',100000, 1),
+       ('Head of Operations',160000, 2),
+       ('General operator', 100500, 2),
+       ('Lead Engineer', 200000, 3),
+       ('Engineer', 90000, 3),
+       ('Sales Manager', 100000, 4),
+       ('Sales', 90500, 4);
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES ('JOHN','DOE', 1, 1),
+       ('STACY', 'LISA', 2, NULL),
+       ('MIGUEL', 'OHARA', 3, 2),
+       ('MILES','MORALES', 4, NULL),
+       ('OLIVE', 'OIL', 5, 3),
+       ('ALEXIUS','SAVAGE',6, NULL),
+       ('MARY','SUE', 7, 4),
+       ('BOBBY','HILL', 8, NULL);         
+
+
+ 
