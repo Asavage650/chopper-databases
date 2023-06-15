233d5cbbd60c124ae43b1c2dc77c700c325d97e3
{alexius <{alexiussavage0@gmail.com}>
2023-06-15 12:34:16 -0700

    starting on seeds file

--- seeds.sql
+++ seeds.sql
@@ -1,2 +1,7 @@
-INSERT INTO employee (name)
-VALUES()
+ SELECT role.id, role.title, role.salary FROM role ORDER BY role.id;
+ SELECT role.id, role.title, FROM role ORDER BY role.id;
+ SELECT * FROM employee;
+
+ SELECT department.id, department.name, FROM department ORDER BY department.id;
+
+ SELECT department.name, AS department, role.title, employee.id, employee
